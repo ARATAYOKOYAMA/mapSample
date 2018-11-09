7c820adfd6fdd42f238c64a04d116e4efa750bcb
ARATAYOKOYAMA <bob2525@hotmail.co.jp>
2018-11-10 01:55:58 +0900

    Initial Commit

---
+++ MapTest/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MapTest
+//
+//  Created by 横山新 on 2018/11/10.
+//  Copyright © 2018年 aratayokoyama. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
